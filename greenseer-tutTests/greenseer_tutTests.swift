77ef4563a7cfcf579ce5ab18976b7fdb2aef2feb
maddyonline <madhavjha@gmail.com>
2018-11-18 11:57:05 -0800

    Initial Commit

---
+++ greenseer-tutTests/greenseer_tutTests.swift
@@ -0,0 +1,34 @@
+//
+//  greenseer_tutTests.swift
+//  greenseer-tutTests
+//
+//  Created by Madhav Jha on 11/18/18.
+//  Copyright © 2018 Madhav Jha. All rights reserved.
+//
+
+import XCTest
+@testable import greenseer_tut
+
+class greenseer_tutTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
