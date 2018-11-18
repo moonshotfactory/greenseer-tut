1f7f2588a97a2353ea3f0c93127920f2f21469f3
maddyonline <madhavjha@gmail.com>
2018-11-18 14:14:44 -0800

    Initial camera setup

--- greenseer-tut/ViewController.swift
+++ greenseer-tut/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+        let captureSession = AVCaptureSession()
+        
+        captureSession.sessionPreset = .photo   // optional
+        
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
     }
 
 
